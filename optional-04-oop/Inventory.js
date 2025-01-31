d8f70e903b87396a83b75c724788d958c42ba0ca
rulshrm <raflysahrul3114@gmail.com>
2025-02-01 03:03:48 +0700

    Implement Inventory and Item classes with methods for adding, removing, and listing items

--- optional-04-oop/Inventory.js
+++ optional-04-oop/Inventory.js
@@ -7,7 +7,27 @@
  * - Memiliki method `listItems` untuk mengembalikan string yang merupakan informasi detail barang (dipanggil dari fungs `item.displayDetails()`).
  */
 
-class Inventory {}
+class Inventory {
+  items = [];
+
+  addItem(item) {
+    // Tambahkan item ke dalam array items
+    this.items.push(item);
+  }
+
+  removeItem(id) {
+    // Hapus item dari array items berdasarkan id
+    const index = this.items.findIndex(item => item.id === id);
+    if(index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  listItems() {
+    // Implementasikan method ini
+    return this.items.map(item => item.displayDetails()).join("\n")
+  }
+}
 
 
 // Jangan hapus kode di bawah ini!
